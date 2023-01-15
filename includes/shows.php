3179ee748c1387cc4e934ab1c2f3badbc937a728
Chris Jones <chris@heyjones.com>
2023-01-15 00:26:50 -0800

    chore: refactor show sync

--- includes/shows.php
+++ includes/shows.php
@@ -3,6 +3,7 @@
 namespace transistor\shows;
 
 add_action( 'init', __NAMESPACE__ . '\\init' );
+add_action( 'rest_api_init', __NAMESPACE__ . '\\rest_api_init' );
 
 function init() {
     register_taxonomy(
@@ -33,70 +34,19 @@ function init() {
             )
         )
     );
-    // Get the show(s) the site owner would like to sync
-    $shows = get_option( 'transistor_shows' );
-    if( ! $shows ) {
-        return;
-    }
-    // Get the existing show(s)
-    $terms = get_terms(
+}
+
+function rest_api_init() {
+    register_rest_route(
+        'transistor/v1',
+        '/episode_published/',
         array(
-            'taxonomy' => 'transistor-show',
-            'fields' => 'ids',
-            'hide_empty' => false,
-            'meta_query' => array(
-                array(
-                    'key' => '_id',
-                    'compare' => 'IN',
-                    'value' => $shows,
-                    'type' => 'NUMERIC',
-                )
-            )
+            'methods' => \WP_REST_Server::EDITABLE,
+            'callback' => __NAMESPACE__ . '\\episode_published',
         )
     );
-    // Identify which show(s) need(s) to be created
-    $shows = array_diff( $shows, $terms );
-    if( ! $shows ) {
-        return;
-    }
-    $transistor_api_key = get_option( 'transistor_api_key' );
-    // Create the show(s)
-    foreach( (array) $shows as $show ) {
-        // Get the term details
-        $request = wp_remote_get(
-            'https://api.transistor.fm/v1/shows/' . $show,
-            array(
-                'headers' => array(
-                    'x-api-key' => $transistor_api_key,
-                )
-            )
-        );
-        if( is_wp_error( $request ) || 200 !== wp_remote_retrieve_response_code( $request ) ) {
-            continue;
-        }
-        $response = json_decode( wp_remote_retrieve_body( $request ) );
-        $show = $response->data;
-        // Create the term
-        $term = wp_insert_term(
-            $show->attributes->title,
-            'transistor-show',
-            array(
-                'slug' => $show->attributes->slug,
-            )
-        );
-        if( ! is_wp_error( $term ) ) {
-            add_term_meta(
-                $term['term_id'],
-                '_id',
-                $show->id,
-                true
-            );
-            add_term_meta(
-                $term['term_id'],
-                '_attributes',
-                $show->attributes,
-                true
-            );
-        }
-    }
+}
+
+function episode_published( $data ) {
+    // Create the episode post
 }
