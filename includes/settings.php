689b57867cb8aa773e0aed9349e54ac1e84d8c0a
Chris Jones <chris@heyjones.com>
2023-01-14 21:57:06 -0800

    feat: sync shows in settings

--- includes/settings.php
+++ includes/settings.php
@@ -1,11 +1,14 @@
 <?php
 
+use \CreateWithRani\Transistor\Transistor;
+
 namespace transistor\settings;
 
 add_action( 'admin_init', __NAMESPACE__ . '\\admin_init' );
 add_action( 'admin_menu', __NAMESPACE__ . '\\admin_menu' );
 
 function admin_init() {
+    // API
     register_setting(
         'transistor',
         'transistor_api_key',
@@ -28,6 +31,25 @@ function admin_init() {
         'transistor',
         'transistor_api',
     );
+    // Shows
+    $transistor_api_key = get_option( 'transistor_api_key' );
+    if( $transistor_api_key ) {
+        register_setting(
+            'transistor',
+            'transistor_shows',
+            array(
+                'type' => 'array',
+                'description' => 'The Transistor shows you would like to sync to your WordPress site.',
+                'show_in_rest' => false,
+            )
+        );
+        add_settings_section(
+            'transistor_shows',
+            'Shows',
+            __NAMESPACE__ . '\\settings_section_shows',
+            'transistor'
+        );
+    }
 }
 
 function admin_menu() {
@@ -77,5 +99,39 @@ function settings_field() {
     ?>
 	<input type="text" name="transistor_api_key" value="<?php echo $transistor_api_key; ?>" size="25" required>
 	<?php
+
+}
+
+function settings_section_shows() {
+    $transistor_api_key = get_option( 'transistor_api_key' );
+    $transistor_shows = get_option( 'transistor_shows' );
+    $request = wp_remote_get(
+        'https://api.transistor.fm/v1/shows',
+        array(
+            'headers' => array(
+                'x-api-key' => $transistor_api_key,
+            )
+        )
+    );
+    if( is_wp_error( $request ) || 200 !== wp_remote_retrieve_response_code( $request ) ) {
+        return;
+    }
+    $response = json_decode( wp_remote_retrieve_body( $request ) );
+    $shows = $response->data;
+    
+    ?>
+    <p>Select which shows you would like to sync to your WordPress site.</p>
+    <?php
+
+    foreach( (array) $shows as $show ) {
+
+        ?>
+        <label>
+            <input type="checkbox" name="transistor_shows[]" value="<?php echo $show->id; ?>" <?php echo in_array( $show->id, $transistor_shows ) ? 'checked' : ''; ?>>
+            <?php echo $show->attributes->title; ?>
+        </label>
+        <?php
+
+    }
 
 }
